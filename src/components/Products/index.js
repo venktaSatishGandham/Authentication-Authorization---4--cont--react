010f223aa5ee889b78edfd02db03fd19815c4b56
satish <satishgandham728@gmail.com>
2022-11-22 15:54:29 +0000

    added exclusive prime deals section

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import AllProductsSection from '../AllProductsSection'
+import PrimeDealsSection from '../PrimeDealsSection/index'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <PrimeDealsSection />
+      <AllProductsSection />
+    </div>
+  </>
+)
+
+export default Products
